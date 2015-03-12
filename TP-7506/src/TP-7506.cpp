d6f08630bf8a2bc2ca26a4bff93f0734fb4d8431
marcosRiarte <mriarte@fi.uba.ar>
2015-03-12 16:28:38 -0300

    Prueba ver

---
+++ TP-7506/src/TP-7506.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : TP-7506.cpp
+// Author      : Marcos Riarte
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
